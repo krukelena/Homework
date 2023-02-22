663dd644a65a6934cf83e0e055581083c4e301a9
Elena Kruk <krukelenka84@gmail.com>
2023-02-22 17:18:49 +0300

    [*][-] refactor

--- Homework_1/Program.cs
+++ Homework_1/Program.cs
@@ -1,4 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-string name = Console.ReadLine();
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello " + name);
+﻿using System;
+
+namespace Homework_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter yor name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
