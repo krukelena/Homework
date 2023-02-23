a42bae737e3ed9142a4041cd8276e9045a14f1d2
Elena Kruk <krukelenka84@gmail.com>
2023-02-24 00:16:57 +0300

    [*] hw2 task2,3

--- HomeWork_2/Program.cs
+++ HomeWork_2/Program.cs
@@ -11,6 +11,8 @@ namespace HomeWork_2
         static void Main(string[] args)
         {
             Task1();
+            Task2();
+            Task3();
         }
 
         //Напишите программу - консольный калькулятор.
@@ -69,6 +71,76 @@ namespace HomeWork_2
                     }
             }
         }
+        static void Task2()
+        {
+            // Напишите программу определения, попадает ли указанное пользователем
+            // число от 0 до 100 в числовой промежуток[0 - 14][15 - 35][36 - 49][50 - 100].
+            //Если да, то укажите, в какой именно промежуток.
+            //Если пользователь указывает число, не входящее ни в один из имеющихся числовых промежутков,
+            //то выводится соответствующее сообщение.
+            Console.Write("Введите число от 0 до 100: ");
+
+            double number = Convert.ToDouble(Console.ReadLine());
+            
+            if ((number >=0) && (number <=14))
+            {
+                Console.WriteLine($"Число от 0 до 14"); ;
+            }
+            else if ((number >= 15) && (number <= 35))
+            {
+                Console.WriteLine($"Число от 15 до 35");
+            }
+            else if ((number >= 36) && (number <= 49))
+            {
+                Console.WriteLine($"Число от 36 до 49");
+            }
+            else if ((number >= 50) && (number <= 100))
+            {
+                Console.WriteLine($"Число от 50 до 100");
+            }
+            else
+            {
+                Console.WriteLine($"Не соответсвует числовым промежуткам");
+             }
+
+
+
+
+        }
+        static void Task3()
+        {
+            //Напишите программу, которая будет выполнять проверку чисел на четность.
+            //Предложите два варианта решения поставленной задачи.
+
+            Console.Write("Введите число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Число четное.");
+            }
+            else
+            {
+                Console.WriteLine("Число нечетное.");
+            }
+
+            //int a = 0;
+            //Console.Write("Введите число: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+           
+            //a = (number % 2);
+
+            //if ( a == 0)
+            //{
+            //    Console.WriteLine("Число четное.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Число нечетное.");
+            //}
+        }
+
+
     }
 }
-
+ 
