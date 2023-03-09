87d2776262e63b893e6333dcb96c6e3327332a7b
Elena Kruk <krukelenka84@gmail.com>
2023-03-09 11:07:41 +0300

    [*] add homework_5 (task 1,2,3)

---
+++ HomeWork_5/ATM.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_5
+{
+    public class ATM
+    {
+        //.3. Создать класс, описывающий банкомат.
+        //Набор купюр, находящихся в банкомате, должен задаваться тремя
+        //свойствами:
+        //количеством купюр номиналом 20, 50 и 100.
+        //Сделать метод для добавления денег в банкомат.
+        //Сделать метод, снимающуий деньги, который принимает сумму денег,
+        //а возвращает булевое значение - успешность выполнения операции.
+        //При снятии денег метод должен распечатывать каким количеством
+        //купюр какого номинала выдаётся сумма.
+        //Создать конструктор с тремя параметрами - количеством купюр каждого
+        //номинала.
+
+        public int TwentyDollarBillsCount;
+        public int FiftyDollarBillsCount;
+        public int HundredDollarBillsCount;
+
+
+        public ATM(int twentyDollarBillsCount, int fiftyDollarBillsCount, int hundredDollarBillsCount)
+        {
+            TwentyDollarBillsCount = twentyDollarBillsCount;
+            FiftyDollarBillsCount = fiftyDollarBillsCount;
+            HundredDollarBillsCount = hundredDollarBillsCount;
+        }
+
+        public void AddMoney(int twentyDollarBillsCount, int fiftyDollarBillsCount, int hundredDollarBillsCount)
+        {
+            TwentyDollarBillsCount += twentyDollarBillsCount;
+            FiftyDollarBillsCount += fiftyDollarBillsCount;
+            HundredDollarBillsCount += hundredDollarBillsCount;
+
+        }
+        public bool WithdrawCash (int cash)
+        {
+            int twentyDollarBillsCount = 0;
+            int fiftyDollarBillsCount = 0;
+            int hundredDollarBillsCount = 0;
+
+            while (cash > 0)
+            {
+                if (cash >= 100)
+                {
+                    cash -= 100;
+                    hundredDollarBillsCount++;
+                }
+                else if (cash >= 50 && cash < 100)
+                {
+                    cash -= 50;
+                    fiftyDollarBillsCount++;
+                }
+                else if (cash >= 20 && cash < 50)
+                {
+                    cash -= 20;
+                    twentyDollarBillsCount++;
+                }
+                else break;
+            }
+            if (cash != 0) return false;
+
+            if (twentyDollarBillsCount!= 0) Console.WriteLine($"Выдано {twentyDollarBillsCount} купюр наминалом 20");
+            if (fiftyDollarBillsCount != 0) Console.WriteLine($"Выдано {fiftyDollarBillsCount} купюр наминалом 50");
+            if (hundredDollarBillsCount != 0) Console.WriteLine($"Выдано {hundredDollarBillsCount} купюр наминалом 100");
+            Console.WriteLine();
+            return true;
+        }
+
+    }
+}
---
+++ HomeWork_5/CreditCard.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_5
+{
+    public class CreditCard
+    {
+        // 2. Создать класс CreditCard c полями номер счета, текущая сумма на счету.
+        //Добавьте метод, который позволяет начислять сумму на кредитную
+        //карточку.
+        //Добавьте метод, который позволяет снимать с карточки некоторую
+        //сумму.
+        //Добавьте метод, который выводит текущую информацию о карточке.
+        //Напишите программу, которая создает три объекта класса CreditCard у
+        //которых заданы номер счета и начальная сумма
+        //Тестовый сценарий для проверки:
+        //Положите деньги на первые две карточки и снимите с третьей.
+        //Выведите на экран текущее состояние всех трех карточек.
+
+
+        public string Number;
+        public double Cash;
+
+        public CreditCard(string number, double cash)
+        {
+            this.Number = number;
+            this.Cash = cash;
+        }
+
+        public void ReplenishCash(double sum)
+        {
+            Cash += sum;
+        }
+
+        public void WithdrawCash(double sum)
+        {
+            Cash -= sum;
+        }
+        public void PrintCreditCard()
+        {
+            Console.WriteLine($"Номер счета: {Number} + Текущая сумма на карте: {Cash}");
+        }
+    }
+
+}
